0da5a845bef275ce76541b6c3e3b105ed36f7904
flores-luis <ricardoluis6212@gmail.com>
2021-08-05 09:15:41 -0700

    creating an array of objects part 1

---
+++ js/object.js
@@ -0,0 +1,16 @@
+/*
+  Create an array of 'pokemon' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+
+const pokemon = [
+    {
+        name: 'Dratini',
+        type: 'Dragon',
+        species: 'Dragon Pokemon',
+        height: (1.8,'m'),
+        weight: (3.3,'kg'),
+        ability: 'Shed Skin'
+    }
+];
