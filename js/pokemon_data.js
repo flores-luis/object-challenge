3fb8241ffe7a620d2f3f7ee2905f9e13796e1d4e
flores-luis <ricardoluis6212@gmail.com>
2021-08-05 21:30:29 -0700

    created a data js file

---
+++ js/pokemon_data.js
@@ -0,0 +1,34 @@
+/*
+  Create an array of 'pokemon' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+
+
+// This is an array of objects
+const pokemon = [
+    {
+        name: 'Dratini',
+        type: 'Dragon',
+        species: 'Dragon Pokemon',
+        height: (1.8,'m'),
+        weight: (3.3,'kg'),
+        ability: 'Shed Skin'
+    },
+    {
+      name: 'Dragonair',
+      type: 'Dragon',
+      species: 'Dragon Pokemon',
+      height: (4.0,'m'),
+      weight: (16.5,'kg'),
+      ability: 'Marvel Scale'
+  },
+  {
+    name: 'Dragonite',
+    type: 'Dragon',
+    species: 'Dragon Pokemon',
+    height: (2.2,'m'),
+    weight: (210,'kg'),
+    ability: 'Inner Focus'
+  }
+];
